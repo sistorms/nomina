173713849899f36f466e0df1a0861fa545bc02a3
Julio Moreno <julio23m.y@gmail.com>
2020-02-28 10:51:46 -0400

    agregar proceso nomina

--- concepto_pago/admin.py
+++ concepto_pago/admin.py
@@ -8,7 +8,33 @@ class VariableAdmin(admin.ModelAdmin):
 
 @admin.register(Formulacion)
 class FormulacionAdmin(admin.ModelAdmin):
-    pass
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(
+            request,
+            extra_context=extra_context,
+        )
+
+        try:
+            qs = response.context_data['cl'].queryset
+        except (AttributeError, KeyError):
+            return response
+
+        metrics = {
+            'total': Count('cod_solicitud'),
+            'total_sales': Sum('cod_solicitud'),
+            
+        }
+
+        response.context_data['summary'] = list(
+            qs
+            .values('status')
+            .annotate(**metrics)
+            .order_by('-status')
+        )
+        response.context_data['summary_total'] = dict(
+            qs.aggregate(**metrics)
+        ) 
+        return response
 
 
 @admin.register(ElementoPago)
---
+++ proceso_nomina/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProcesoNominaConfig(AppConfig):
+    name = 'proceso_nomina'
